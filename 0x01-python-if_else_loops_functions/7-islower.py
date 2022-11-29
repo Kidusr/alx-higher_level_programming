19279d562f645f8eebf012a46f3fe00de85d0486
Kidusr <kidusr69@gmail.com>
2022-11-29 11:43:37 -0800

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,9 +3,10 @@
 # Author - Kidus Teshome
 
 def islower(c):
+
     """Function checks for lowercase characters."""
 
-if ord(c) >= 97 and ord(c) <= 122:
-    return True
-else:
-    return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
