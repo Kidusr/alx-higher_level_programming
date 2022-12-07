e344b42a7258a0d08fc71686bb3b6af2924a726d
Kidusr <kidusr69@gmail.com>
2022-12-07 12:14:30 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,5 +6,4 @@ def best_score(a_dictionary):
 
         return (None)
 
-
     return (max(a_dictionary, key=a_dictionary.get))
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,7 +4,6 @@ def complex_delete(a_dictionary, value):
 
     list_keys = list(a_dictionary.keys())
 
-
     for value_dic in list_keys:
 
         if value == a_dictionary.get(value_dic):
